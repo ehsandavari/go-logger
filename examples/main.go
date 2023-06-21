6473deef907e2f6c338568e4207c7128e3da969f
ehsan <ehsan.d@780.ir>
2023-06-21 18:11:05 +0330

    example

--- examples/main.go
+++ examples/main.go
@@ -24,7 +24,7 @@ func main() {
 	ctx = context.WithValue(ctx, "requestId", "valtest")
 	ctx = context.WithValue(ctx, "traceId", "asdfasdf24321")
 
-	url := "https://google.com?sad=asd&fsdg=asd"
+	url := "https://github.com/ehsandavari"
 
 	payload := strings.NewReader("{\n\t\"isPin\": true\n}")
 
@@ -32,14 +32,18 @@ func main() {
 
 	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("Authorization", "Bearer eyJhbGciOiJFUzUxMiIsImtpZCI6IjllYzgzYmUxLWVmMjctNDA2OC1iYTUzLTk5MWY0MWI0OWYxYiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJXRUIiLCJhdXRob3JpdGllcyI6bnVsbCwiY2xpZW50IjoidW5raG93biIsImV4cCI6MTY4MjQzOTQ0MCwiaXNzIjoiIiwianRpIjoiNzEzOTE5MDAtNTA1Ny00N2E1LWI5ODktM2Q0MGI5MmRhOTdkIiwibW9iaWxlIjoiOTg5MjE1NTgwNjkwIiwibW9iaWxlX25vIjoiRWtyVlVHdmptRkhmb0MzYVhSQnZBNFE4WDBZbDNoNnJ1WWpGQ1E9PSIsIm5iZiI6MTY4MjQzMTY0MCwibm9uX290cCI6IiIsInBlcm1pc3Npb25zIjpudWxsLCJzY29wZSI6IndlYiIsInN1YiI6IjIxNDcwODJhLTA3OWEtNDA4Yy05OGE4LWFhYzExMmZiZjM3ZSJ9.ATg2NbeJoIOvUXts5n3sJwy-AAK_DN3tF02F7djtLopAIOQ3jxgfdcfXWJ4sTWo7zba4cNt0OjDgbMPOw6nDDMLlAYLYt6rOznMpUCCqrzxe5NJ32taa9fBhsOrk0NIGTMcqf0q5E4Ey90avuBCnD4iEUxzprvBr1To0xEzNHwGHdrw4")
+
 	iLogger.WithBool("key", true).WithError(errors.New("sadadsadsasdasd")).WithHttpRequest(req).Error(ctx, "asdad")
 
 	res, _ := http.DefaultClient.Do(req)
 
+	iLogger.WithBool("key", true).WithError(errors.New("sadadsadsasdasd")).WithHttpRequest(req).Error(ctx, "asdad")
+	iLogger.WithBool("key", true).WithError(errors.New("sadadsadsasdasd")).WithHttpResponse(res).Error(ctx, "asdad")
+	iLogger.WithBool("key", true).WithError(errors.New("sadadsadsasdasd")).WithHttpRequest(req).WithHttpResponse(res).Error(ctx, "asdad")
 	name := NewName("1", "2", "3")
 	iLogger.WithAny("Sname", name).WithBool("key", true).WithError(errors.New("sadadsadsasdasd")).WithHttpResponse(res).Debug(ctx, "asdad")
-
 	iLogger.Warn(ctx, "asdasdasdasd")
+
 	if err := iLogger.Sync(); err != nil {
 	}
 }
